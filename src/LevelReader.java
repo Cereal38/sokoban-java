151cf9ebb3dc7fc2c77bf21d03f01761cdb7fc3f
Cereal38 <guillaume.bruncosmegazot@gmail.com>
2024-01-17 14:43:40 +0100

    The level read and print the all file level

--- src/LevelReader.java
+++ src/LevelReader.java
@@ -1,9 +1,23 @@
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 
 public class LevelReader {
 
   public Level readNextLevel(File source) {
 
-    return new Level();
+    Level level = new Level();
+    String line = "";
+
+    try (Scanner scanner = new Scanner(source)) {
+      while (scanner.hasNextLine()) {
+        line = scanner.nextLine();
+        System.out.println(line);
+      }
+    } catch (FileNotFoundException e) {
+      e.printStackTrace();
+    }
+
+    return level;
   }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,36 +1,11 @@
-// import java.util.Scanner;
-// import java.util.NoSuchElementException;
-// import java.util.InputMismatchException;
+import java.io.File;
 
 class Main {
   public static void main(String[] args) {
 
-    Level level = new Level();
-    level.setName("1");
-    System.out.println(level.name());
+    LevelReader reader = new LevelReader();
+    File file = new File("levels/original.txt");
+    reader.readNextLevel(file);
 
-    // Scanner scanner;
-    // int input = 0;
-
-    // scanner = new Scanner(System.in);
-
-    // boolean validInt = false;
-    // while (!validInt) {
-    // System.out.println("Give a number: ");
-    // try {
-    // input = scanner.nextInt();
-    // validInt = true;
-    // } catch (InputMismatchException e) {
-    // System.out.println("This is not a number...");
-    // scanner.next();
-    // } catch (NoSuchElementException e) {
-    // System.out.println("You wrote nothing!");
-    // scanner.next();
-    // }
-    // }
-
-    // System.out.println("You said: " + input);
-
-    // scanner.close();
   }
 }
