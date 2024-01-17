8dcce30f37922d5c9f933301a912e4a79c8337bc
Cereal38 <guillaume.bruncosmegazot@gmail.com>
2024-01-17 14:04:52 +0100

    Add useful methods to Level class

--- src/Level.java
+++ src/Level.java
@@ -8,7 +8,30 @@ class Level {
     this.name = name;
   }
 
-  public void videCase(int column, int row) {
+  public void emptyCell(int column, int row) {
     grid[column][row] = ' ';
+  }
+
+  public void addWall(int column, int row) {
+    grid[column][row] = '#';
+  }
+
+  public void addPlayer(int column, int row) {
+    grid[column][row] = '@';
+  }
+
+  public void addGoal(int column, int row) {
+    grid[column][row] = '.';
+  }
+
+  public int columns() {
+    return this.grid.length;
+  }
+
+  public int rows() {
+    if (this.columns() == 0) {
+      return 0;
+    }
+    return this.grid[0].length;
   }
 }
