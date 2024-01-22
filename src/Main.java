d78c8ca75cbc2bd60da7b659c76c689c59d0825c
Cereal38 <guillaume.bruncosmegazot@gmail.com>
2024-01-22 15:51:43 +0100

    Insert shift and head working

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,29 @@
-import java.io.File;
-
 class Main {
   public static void main(String[] args) {
 
-    /* Load all level from the given file */
-    File fileIn = new File("levels/original.txt");
-    LevelReader reader = new LevelReader(fileIn);
-    LevelWriter writer = new LevelWriter(System.out);
-    Level level = reader.readNextLevel();
+    // /* Load all level from the given file */
+    // File fileIn = new File("levels/original.txt");
+    // LevelReader reader = new LevelReader(fileIn);
+    // LevelWriter writer = new LevelWriter(System.out);
+    // Level level = reader.readNextLevel();
 
-    while (level != null) {
-      writer.writeLevel(level);
-      level = reader.readNextLevel();
-    }
+    // while (level != null) {
+    // writer.writeLevel(level);
+    // level = reader.readNextLevel();
+    // }
+    Sequence sequence = new Sequence();
+    sequence.printSequence();
+    sequence.insertHead(3);
+    sequence.printSequence();
+    sequence.insertHead(4);
+    sequence.printSequence();
+    sequence.insertHead(5);
+    sequence.printSequence();
+    sequence.insertTail(2);
+    sequence.printSequence();
+    sequence.insertTail(1);
+    sequence.printSequence();
+    sequence.insertHead(6);
+    sequence.printSequence();
   }
 }
--- src/Sequence.java
+++ src/Sequence.java
@@ -1,26 +1,42 @@
 public class Sequence {
 
-  int tab[] = new int[3];
+  int tab[] = new int[10];
   int head = 0;
   int tail = 0;
 
+  // Write and shift head right
   void insertHead(int value) {
+
     tab[head] = value;
 
-    int nexthead = head == tab.length - 1 ? 0 : head + 1;
+    int nextHead = head == tab.length - 1 ? 0 : head + 1;
 
-    if (nexthead == tail) {
+    if (nextHead == tail) {
       throw new RuntimeException("Sequence is full");
+    } else {
+      head = nextHead;
     }
+  }
 
-    head = nexthead;
+  // Shift tail left and write
+  void insertTail(int value) {
+
+    int nextTail = tail == 0 ? tab.length - 1 : tail - 1;
+
+    if (nextTail == head) {
+      throw new RuntimeException("Sequence is full");
+    } else {
+      tail = nextTail;
+      tab[tail] = value;
+    }
   }
 
   void printSequence() {
     int i = tail;
     while (i != head) {
-      System.out.println(tab[i]);
+      System.out.print(tab[i] + " ");
       i = i == tab.length - 1 ? 0 : i + 1;
     }
+    System.out.println();
   }
 }
