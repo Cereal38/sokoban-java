0679d5e8b1b74a4cabffcafdd2f375b875fe6d2e
Cereal38 <guillaume.bruncosmegazot@gmail.com>
2024-01-22 16:17:18 +0100

    Simplify the code and enhance the algo for sequence

--- src/Sequence.java
+++ src/Sequence.java
@@ -1,7 +1,7 @@
 public class Sequence {
 
   int tab[] = new int[10];
-  int start = 0;
+  int start = tab.length - 1;
   int end = 0;
 
   // Write and increase end
@@ -10,30 +10,30 @@ public class Sequence {
     tab[end] = value;
 
     int nextEnd = end == tab.length - 1 ? 0 : end + 1;
+    end = nextEnd;
 
     if (nextEnd == start) {
-      throw new RuntimeException("Sequence is full");
-    } else {
-      end = nextEnd;
+      grow();
     }
 
   }
 
-  // Write and shift head right
+  // Write and decrease start
   void insertStart(int value) {
 
+    tab[start] = value;
+
     int nextStart = start == 0 ? tab.length - 1 : start - 1;
+    start = nextStart;
 
     if (nextStart == end) {
-      throw new RuntimeException("Sequence is full");
-    } else {
-      start = nextStart;
-      tab[start] = value;
+      grow();
     }
   }
 
   void printSequence() {
-    int i = start;
+    System.out.print("Sequence: ");
+    int i = start == tab.length - 1 ? 0 : start + 1;
     while (i != end) {
       System.out.print(tab[i] + " ");
       i = i == tab.length - 1 ? 0 : i + 1;
@@ -42,5 +42,18 @@ public class Sequence {
   }
 
   private void grow() {
+    int newTab[] = new int[tab.length * 2];
+    int i;
+    for (i = 0; i < start; i++) {
+      newTab[i] = tab[i];
+    }
+    start = i + 1;
+
+    for (i = tab.length - 1; i >= end; i--) {
+      newTab[newTab.length - 1 - i] = tab[i];
+    }
+    end = newTab.length - 1 - i;
+
+    tab = newTab;
   }
 }
