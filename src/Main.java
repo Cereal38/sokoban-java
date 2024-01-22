0f38345522a43d25345ebf2fea98484b50dbe51e
Cereal38 <guillaume.bruncosmegazot@gmail.com>
2024-01-22 16:27:32 +0100

    Clear main

--- src/Main.java
+++ src/Main.java
@@ -1,43 +1,17 @@
+import java.io.File;
+
 class Main {
   public static void main(String[] args) {
 
-    // /* Load all level from the given file */
-    // File fileIn = new File("levels/original.txt");
-    // LevelReader reader = new LevelReader(fileIn);
-    // LevelWriter writer = new LevelWriter(System.out);
-    // Level level = reader.readNextLevel();
+    /* Load all level from the given file */
+    File fileIn = new File("levels/original.txt");
+    LevelReader reader = new LevelReader(fileIn);
+    LevelWriter writer = new LevelWriter(System.out);
+    Level level = reader.readNextLevel();
 
-    // while (level != null) {
-    // writer.writeLevel(level);
-    // level = reader.readNextLevel();
-    // }
-    Sequence sequence = new Sequence();
-    sequence.printSequence();
-    sequence.insertEnd(3);
-    sequence.printSequence();
-    sequence.insertEnd(4);
-    sequence.printSequence();
-    sequence.insertEnd(5);
-    sequence.printSequence();
-    sequence.insertStart(2);
-    sequence.printSequence();
-    sequence.insertStart(1);
-    sequence.printSequence();
-    sequence.insertEnd(6);
-    sequence.printSequence();
-    sequence.removeEnd();
-    sequence.printSequence();
-    sequence.removeEnd();
-    sequence.printSequence();
-    sequence.removeEnd();
-    sequence.printSequence();
-    sequence.removeStart();
-    sequence.printSequence();
-    sequence.removeStart();
-    sequence.printSequence();
-    sequence.removeStart();
-    sequence.printSequence();
-    sequence.removeStart();
-    sequence.printSequence();
+    while (level != null) {
+      writer.writeLevel(level);
+      level = reader.readNextLevel();
+    }
   }
 }
